2232c26cb4212d18b6d0313b12c83f2054893e58
BloodXStillCool <125414068+BloodXStillCool@users.noreply.github.com>
2023-08-26 23:09:41 -0700

    Update main.js

--- main.js
+++ main.js
@@ -28,6 +28,6 @@ function validateCaptcha() {
       errCaptcha.innerHTML = "Wrong captcha";
     } else {
       errCaptcha.innerHTML = "Done";
-      location.replace("https://pastebin.com/raw/wD431s4T")
+      location.replace("https://pastebin.com/raw/RdHXdJnU")
     }
   }
